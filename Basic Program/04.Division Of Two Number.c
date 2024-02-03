80e2641bffe5e3f808d428880cdc8faa9702016e
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-03 22:01:07 +0530

    practice

---
+++ Basic Program/04.Division Of Two Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num1 , Num2 , Div = 0;
+
+    printf("\n ====================Division================== \n");
+    printf("\n Enter 1st number : ");
+    scanf("%d",&Num1);
+    printf("\n Enter 2nd number : ");
+    scanf("%d",&Num2);
+    Div = Num1/Num2;
+    printf("\n==============================================\n");
+    printf("\n Division of Two Numbers : %d",Div);
+    scanf("%d",&Div);
+
+    return 0 ;
+}
+
+
