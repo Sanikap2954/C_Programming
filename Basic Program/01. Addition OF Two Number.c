ca073e043bc4e7e7a68fd653f69ee99e5fce54c8
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-03 10:11:40 +0530

    Practice

---
+++ Basic Program/01. Addition OF Two Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num1 , Num2 , Sum = 0;
+
+    printf("\n ====================Addition================== \n");
+    printf("\n Enter 1st number : ");
+    scanf("%d",&Num1);
+    printf("\n Enter 2nd number : ");
+    scanf("%d",&Num2);
+    Sum= Num1+Num2;
+    printf("\n==============================================\n");
+    printf("\n Addition of Two Numbers : %d",Sum);
+    scanf("%d",&Sum);
+
+    return 0 ;
+}
+
+
