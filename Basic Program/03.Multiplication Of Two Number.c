8b3cf668500213f2b1d1697ca85c06e845f30992
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-03 10:28:20 +0530

    Practice

---
+++ Basic Program/03.Multiplication Of Two Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num1 , Num2 , Multi = 0;
+
+    printf("\n ====================Multiplication================== \n");
+    printf("\n Enter 1st number : ");
+    scanf("%d",&Num1);
+    printf("\n Enter 2nd number : ");
+    scanf("%d",&Num2);
+    Multi= Num1*Num2;
+    printf("\n==============================================\n");
+    printf("\n Multiplication of Two Numbers : %d",Multi);
+    scanf("%d",&Multi);
+
+    return 0 ;
+}
+
+
