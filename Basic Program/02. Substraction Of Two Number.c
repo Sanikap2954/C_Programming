20f44f4b8860a8997f26fdaaaa8908020f9210a5
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-03 10:22:58 +0530

    Practice

---
+++ Basic Program/02. Substraction Of Two Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num1 , Num2 , Sub = 0;
+
+    printf("\n ====================Substraction================== \n");
+    printf("\n Enter 1st number : ");
+    scanf("%d",&Num1);
+    printf("\n Enter 2nd number : ");
+    scanf("%d",&Num2);
+    Sub = Num1-Num2;
+    printf("\n==============================================\n");
+    printf("\n Substraction of Two Numbers : %d",Sub);
+    scanf("%d",&Sub);
+
+    return 0 ;
+}
+
+
