e00acbd962331b2432026e80e55128bc3065e66a
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-04 20:33:38 +0530

    Practice

---
+++ Control Statement & Loop/03.Positive & Negative Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+    int main()
+    {
+        int num ;
+        printf("Enter the number :");
+        scanf("%d",&num);
+
+        if(num>0)
+        {
+            printf("\n Number is positive :\n");
+        }
+        else
+        {
+             printf(" \n Number is negetive :\n");
+        }
+    }
