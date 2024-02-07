c454af202a5c5c67ef04f708cedee1074208e528
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-07 22:21:30 +0530

    practice

---
+++ Basic Program/07.Square Of Number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int Num;
+    printf("Enter a Number:");
+    scanf("%d",&Num);
+
+     int Square = Num * Num;
+    printf(" Square of %d is : %d \n",Num,Square);
+
+    return 0;
+}
