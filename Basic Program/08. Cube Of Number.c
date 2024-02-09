6f6363f22318f6471701fcaa77a777f08b44d641
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-09 20:47:02 +0530

    practice

---
+++ Basic Program/08. Cube Of Number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int Num;
+    printf("Enter a Number:");
+    scanf("%d",&Num);
+
+     int Cube = Num * Num * Num;
+    printf(" Square of %d is : %d \n",Num,Cube);
+
+    return 0;
+}
