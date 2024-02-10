9168d05444f254ed98d8a924136a20a5aed1a548
Sanikap2954 <sanikapisal2954@gmail.com>
2024-02-10 11:16:42 +0530

    Practice

---
+++ Array/01.Array with initialization & display.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+           int Value[5]= {23,85,64,88,26};
+
+           printf("\n 1st  Element = %d",Value[0]);
+           printf("\n 2nd  Element = %d",Value[1]);
+           printf("\n 3rd  Element = %d",Value[2]);
+           printf("\n 4th  Element = %d",Value[3]);
+           printf("\n 5th  Element = %d",Value[4]);
+
+           getch();
+           return 0;
+ }
